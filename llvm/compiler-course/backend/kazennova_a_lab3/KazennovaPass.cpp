398448c38f03394c6de641a5f71a1149dc629708
AnastasiaKazenn <az89063674225az@gmail.com>
2026-05-02 13:38:32 +0300

    add tests + fix problem

--- llvm/compiler-course/backend/kazennova_a_lab3/KazennovaPass.cpp
+++ llvm/compiler-course/backend/kazennova_a_lab3/KazennovaPass.cpp
@@ -1,75 +1,51 @@
 #include "X86.h"
 #include "X86InstrInfo.h"
 #include "X86Subtarget.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Support/raw_ostream.h"
+#include "MCTargetDesc/X86BaseInfo.h"
 
 using namespace llvm;
 
 namespace {
+
 class NullCheckPass : public MachineFunctionPass {
 public:
   static char ID;
   NullCheckPass() : MachineFunctionPass(ID) {}
 
   bool runOnMachineFunction(MachineFunction &MF) override {
-    const X86Subtarget &STI = MF.getSubtarget<X86Subtarget>();
-    const X86InstrInfo *TII = STI.getInstrInfo();
     bool Changed = false;
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
 
-    LLVMContext &Ctx = MF.getFunction().getContext();
-    Function *AbortFunc = MF.getFunction().getParent()->getFunction("abort");
-    if (!AbortFunc) {
-      FunctionType *FTy = FunctionType::get(Type::getVoidTy(Ctx), false);
-      AbortFunc = Function::Create(FTy, GlobalValue::ExternalLinkage, "abort",
-                                   MF.getFunction().getParent());
-    }
+    for (MachineBasicBlock &MBB : MF) {
+      for (auto MI = MBB.begin(); MI != MBB.end(); ++MI) {
 
-    for (auto &MBB : MF) {
-      SmallVector<MachineInstr *, 8> MemInsts;
-      for (auto &MI : MBB)
-        if (MI.mayLoad() || MI.mayStore())
-          MemInsts.push_back(&MI);
-
-      for (MachineInstr *MI : MemInsts) {
-        unsigned PtrReg = 0;
-        for (const MachineOperand &MO : MI->operands()) {
-          if (MO.isReg() && MO.isUse() && !MO.isDef()) {
-            PtrReg = MO.getReg();
-            break;
-          }
-        }
-        if (!PtrReg)
+        if (!MI->mayLoad() && !MI->mayStore())
           continue;
 
-        MachineBasicBlock *CheckBB = &MBB;
-        MachineBasicBlock *ContBB = MF.CreateMachineBasicBlock();
-        MachineBasicBlock *TrapBB = MF.CreateMachineBasicBlock();
+        const MCInstrDesc &Desc = MI->getDesc();
+        int MemOpNo = X86II::getMemoryOperandNo(Desc.TSFlags);
+        if (MemOpNo < 0)
+          continue;
+        MemOpNo += X86II::getOperandBias(Desc);
 
-        MF.insert(std::next(CheckBB->getIterator()), ContBB);
-        MF.insert(std::next(ContBB->getIterator()), TrapBB);
+        const MachineOperand &BaseOp = MI->getOperand(MemOpNo + X86::AddrBaseReg);
+        if (!BaseOp.isReg())
+          continue;
+        Register BaseReg = BaseOp.getReg();
+        if (!BaseReg.isValid())
+          continue;
 
-        ContBB->splice(ContBB->end(), CheckBB, std::next(MI->getIterator()),
-                       CheckBB->end());
-        ContBB->transferSuccessors(CheckBB);
+        if (BaseReg == X86::RSP || BaseReg == X86::RBP || BaseReg == X86::RIP)
+          continue;
 
-        auto InsertPos = MI->getIterator();
         DebugLoc DL = MI->getDebugLoc();
-
-        BuildMI(*CheckBB, InsertPos, DL, TII->get(X86::TEST64rr))
-            .addReg(PtrReg)
-            .addReg(PtrReg);
-        BuildMI(*CheckBB, InsertPos, DL, TII->get(X86::JCC_1))
-            .addImm(X86::COND_E)
-            .addMBB(TrapBB);
-        BuildMI(*CheckBB, InsertPos, DL, TII->get(X86::JMP_1)).addMBB(ContBB);
-
-        BuildMI(*TrapBB, TrapBB->end(), DL, TII->get(X86::CALL64pcrel32))
-            .addGlobalAddress(AbortFunc);
-        BuildMI(*TrapBB, TrapBB->end(), DL, TII->get(X86::RET64));
+        BuildMI(MBB, MI, DL, TII->get(TargetOpcode::COPY), X86::RDI)
+            .addReg(BaseReg);
+        BuildMI(MBB, MI, DL, TII->get(X86::CALL64pcrel32))
+            .addExternalSymbol("__verify_pointer");
 
         Changed = true;
       }
@@ -77,9 +53,10 @@ public:
     return Changed;
   }
 };
+
 char NullCheckPass::ID = 0;
 
 static RegisterPass<NullCheckPass>
-    X("null-check", "Insert null check before pointer dereference", false,
-      false);
+    X("null-check", "Insert NULL check before pointer dereference", false, false);
+
 } // namespace
